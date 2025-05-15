ad042f0d064b1f4718e8e4a9c24d6015276836e6
coffeemuggler <mdietze@gfz-potsdam.de>
2025-05-15 14:31:40 +0200

    package cleaning and updating after functions were added

--- R/topotoolboxr-package.R
+++ R/topotoolboxr-package.R
@@ -1,5 +1,24 @@
 ## usethis namespace: start
 #' @useDynLib topotoolboxr, .registration = TRUE, .fixes = "C_"
+#' 
+#' @importFrom terra res values crds crs ext rast project aggregate
+#' @importFrom grDevices heat.colors topo.colors terrain.colors
+#' 
 ## usethis namespace: end
 
 NULL
+
+#' A digital elevation model of the Big Tujunga catchment 
+#'
+#' The dataset contains a wrapped SpatRaster data set, representing a 
+#' digital elevation model (DEM) of the Big Tujunga catchment, USA. After 
+#' loading the example data, it needs to be unwrapped in order to use it.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name srtm_bigtujunga30m_utm11
+#' @usage data(srtm_bigtujunga30m_utm11)
+#' @format A SpatRaster with 643 rows and 1197 columns, referenced in the 
+#' CRS UTM zone 11N.
+NULL
+
