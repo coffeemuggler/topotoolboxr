dd707cea892de01543119e3d33b8da11229a8a40
coffeemuggler <mdietze@gfz-potsdam.de>
2025-05-15 14:29:19 +0200

    functions removed that will get preplaced

--- R/has_topotoolbox.R
+++
@@ -1,9 +0,0 @@
-#' has_topotoolbox
-#'
-#' It is a dummy function that allows us to check for the inclusion of our Src C-functions.
-#'
-#' @export
-has_topotoolbox <- function(){
-    a <- .C(C_wrap_has_topotoolbox,a=as.integer(0))$a
-    return(a == 1)
-}
